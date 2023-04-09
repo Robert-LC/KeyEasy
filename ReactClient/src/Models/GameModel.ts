be768e0cc869f21624cd4ec2e4a6448bf909c806
Robert LoCicero <robertlocicerojr@gmail.com>
2023-04-09 17:27:40 -0400

    Created GameModel.tsx

---
+++ ReactClient/src/Models/GameModel.ts
@@ -0,0 +1,36 @@
+import ScaleType from "../enums/ScaleType";
+import ScaleService from "../services/ScaleService";
+import ScaleModel from "./ScaleModel";
+
+class GameModel {
+    constructor(scaleType: ScaleType, scaleSvc: ScaleService) {
+        this.scaleSvc = scaleSvc;
+        this.scalesLeftToGuess = scaleSvc.GenerateScales(scaleType);
+        this.maxScore = this.calculateMaxScore();
+        this.currentScale = scaleSvc.SelectRandomScale(this.scalesLeftToGuess)
+        this.currentScore = 0;
+    }
+
+
+    public currentScore: number;
+    public maxScore?: number; 
+    public scalesLeftToGuess: ScaleModel[];
+    public currentScale: ScaleModel | undefined;
+    public currentNote?: string;
+    public currentNoteIndex?: number;
+
+    private scaleSvc: ScaleService;
+
+    //=======METHODS=======
+    private calculateMaxScore(): number {
+        return this.scalesLeftToGuess.reduce((total: number, scale: ScaleModel) => total + scale.Notes.length, 0);
+    }
+
+    public selectNewScale(): void {
+        this.currentScale = this.scaleSvc.SelectRandomScale(this.scalesLeftToGuess);
+    }
+    //======================
+
+}
+
+export default GameModel;
